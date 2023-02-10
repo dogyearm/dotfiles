92d7b39fb841e73f7276d7dd2707b22b65c3b9dd
dogyearm <dogyear.iwmt@gmail.com>
2023-02-10 20:33:28 +0900

    remove unused conf

--- bin/autorm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# cp ~/dotfiles/etc/do6year.autorm.plist ~/Library/LaunchAgents/
-# launchctl load ~/Library/LaunchAgents/do6year.autorm.plist
-
-rm -rf $HOME/.Trash/*
-rm -rf $HOME/Downloads/
-rm -rf $HOME/Desktop/*
