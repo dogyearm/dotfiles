6e0917fbdab20f9f19e619e66b9fde5a1d6ff08f
dogyearm <dogyear.iwmt@gmail.com>
2023-02-12 15:55:18 +0900

    update completion.zsh

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -1,5 +1,5 @@
 zstyle ':completion:*:default' menu select=2
-zstyle ':completion:*' list-colors 'di=34' 'ln=35' 'so=32' 'ex=31' 'bd=46;34' 'cd=43;34'
+zstyle ':completion:*' list-colors '${LS_COLORS}'
 zstyle ':completion:*' verbose yes
 zstyle ':completion:*' format '%B%d%b'
 zstyle ':completion:*:warnings' format 'No matches for: %d'
