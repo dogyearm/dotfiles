d2eadac28f63a9a1980de42e0945852914efce3b
dogyearm <dogyear.iwmt@gmail.com>
2023-02-13 10:29:33 +0900

    change vimrc dir

--- etc/link.sh
+++ etc/link.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Symlink
-dotfiles=(.zprofile .zshrc .zshenv .zsh .vimrc .tmux.conf .gitconfig .gitignore .bin)
+# NOTE: change all dotfiles links
+dotfiles=(.zprofile .zshrc .zshenv .zsh .vim .vimrc .tmux.conf .gitconfig .gitignore .bin)
 
 for file in "${dotfiles[@]}"; do
   sourceFile="$(pwd)/$file"
